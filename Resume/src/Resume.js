6bea612afef54cdc001a60f866ea1da533322953
unknown <mansivanani176@gmail.com>
2024-03-29 09:37:03 +0530

    Resume Template

---
+++ Resume/src/Resume.js
@@ -0,0 +1,91 @@
+import img from './img1.jpg'
+function Resume() {
+    return (
+        <>
+            <div style={{ "width": "20%", "backgroundColor": "#987A7F", "color": "white", "float": "left", "marginright": "20px", "height": "985px" }}>
+                <img src={img} alt="" style={{ "width": "90px", "height": "80px", "border": "2px solid pink", "marginLeft": "160px", "marginTop ": "20px" }} />
+                <h5 style={{ "padding-left": "15px", "marginBottom": "-20PX" }}>  New DelhiIndia 110034</h5>
+                <h5 style={{ "padding-left": "15px", "marginBottom": "-20px" }}>+91 22 5555 33</h5>
+                <h5 style={{ "padding-left": "15px", "marginBottom": "-5PX" }}>i.agarwel@sample.ir</h5>
+                <hr style={{ "padding-left": "15px", "width": "91%" }}></hr>
+                <h2 style={{ "padding-left": "15px" }}>PROFESSIONAL SUMMARY</h2>
+                <p style={{ "padding-left": "15px" }}>customer focused retail & sale professional with in-depth understanding of retail dynamics marketing and customer service offering 5 years of experience providing quvality product recommendations and solutions to meet customer expectation and needs. demonstrated record of exceeding revenue targets leveraging sales expertise an communication skills.
+                </p>
+                <hr style={{ "padding-left": "15px", "width": "91%" }}></hr>
+                <h2 style={{ "padding-left": "15px" }}>CORE QUALIFICATIONS</h2>
+                <ul>
+                    <li>cash register opratior</li>
+                    <li>POS system opertion</li>
+                    <li>sales expertise</li>
+                    <li>teamwork</li>
+                    <li>inventory management</li>
+                    <li>accurate money handling</li>
+                    <li>documention & record</li>
+                    <li>keeping</li>
+                </ul>
+                <hr style={{ "width": "91%" }}></hr>
+                <h2 style={{ "padding-left": "15px" }}>LANGUAGES</h2>
+                <ul>
+                    <li>hindu nativ</li>
+                    <li>english nativ</li>
+                    <li>bengali intermediate</li>
+                </ul>
+                <hr style={{ "width": "91%" }}></hr>
+                <h2 style={{ "padding-left": "15px" }}>ADDITIONAL INFORMATION</h2>
+                <ul>
+                    <li>taught lifesaving skills and cpi classes</li>
+                    <li>red cross volunteer</li>
+                </ul>
+            </div>
+            {/* second part */}
+            <div style={{ "width": "40%", "backgroundColor": "#F1ECF0", "color": "whitBLACKe", "marginleft": "20px", "float": "left" }}>
+                <h1 style={{ "padding-left": "15px", "color": "#987A7F ", "fontFamily": "monospace", "fontSize": "50px" }}>IshaanAgarwal</h1>
+                <hr style={{ "width": "97.5%", "color": "#987A7F " }} ></hr>
+                <h3 style={{ "padding-left": "15px" }}>EXPERIENCE</h3>
+                <p style={{ "padding": "15px", "marginTop": "-20PX" }}>February 2021 current</p>
+                <p style={{
+                    "padding-left": "15px", "marginTop": "-30px"
+                }}>retail slaes associateZARAI new Delhi, india</p>
+                < ul style={{ "padding-left": "50px" }}>
+                    <li>customer focused retail & sale professional with in-depth understanding customer focused retail</li>
+                    <li>customer focused retail & sale professional with in-depth understanding customer focused retail</li>
+                    <li>customer focused retail & sale professional with in-depth understanding customer focused retail</li>
+                </ul>
+
+                <p style={{ "padding-left": "15px" }}>march 2019 current</p>
+                <p style={{ "padding-left": "15px", "marginTop": "-15px" }}>retail slaes associateZARAI new Delhi, india</p>
+                <ul style={{ "padding-left": "50px" }}>
+                    <li>customer focused retail & sale professional with in-depth understanding customer focused retail</li>
+                    <li>customer focused retail & sale professional with in-depth understanding customer focused retail</li>
+                    <li>customer focused retail & sale professional with in-depth understanding customer focused retail</li>
+                </ul>
+
+                <p style={{ "padding-left": "15px" }}>july 2019, june</p>
+                <p style={{ "padding-left": "15px", "marginTop": "-15px" }}>retail slaes associateZARAI new Delhi, india</p>
+                <ul style={{ "padding-left": "50px" }}>
+                    <li>customer focused retail & sale professional with in-depth understanding customer focused retail</li>
+                    <li>customer focused retail & sale professional with in-depth understanding customer focused retail</li>
+                    <li>customer focused retail & sale professional with in-depth understanding customer focused retail</li>
+                    <li>customer focused retail & sale professional with in-depth</li>
+                    <li>customer focused retail & sale professional with in-depth</li>
+                </ul>
+                <hr style={{ "padding-left": "15px", "width": "97.5%", "color": "#987A7F " }}></hr>
+                <h4 style={{ "padding-left": "15px" }}>EDUCATION</h4>
+                <p style={{ "padding-left": "15px", "marginTop": "-15px" }}>2018</p>
+                <p style={{ "padding-left": "15px", "marginTop": "-15px" }}>Financial Accounting</p>
+                <p style={{ "padding-left": "15px", "marginTop": "-15px" }}>erhdskjghfskhgfekdsjzvndsdfghdfj</p>
+                <hr style={{ "padding-left": "15px", "width": "97.5%", "backgroundColor": "#987A7F " }}></hr>
+                <h4 style={{ "padding-left": "15px" }}>INTERESTS</h4>
+                <p style={{ "padding-left": "15px" }}>Recreational Football League:</p>
+                <ul style={{ "padding-left": "50PX" }}>
+                    <li>customer focused retail </li>
+                    <li>customer focused retail </li>
+                    <li>team caption</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+
+export default Resume;
